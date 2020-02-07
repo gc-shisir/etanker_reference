fbb864ca3a85da000fe583e98bc966f0ad1d534c
Shisir GC <shisir.gc44@gmail.com>
2020-02-07 17:37:00 +0545

    added spinner

--- app/src/main/java/com/example/etanker/registerActivity.java
+++ app/src/main/java/com/example/etanker/registerActivity.java
@@ -3,24 +3,38 @@ package com.example.etanker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class registerActivity extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner selectCategory;
-    String[] categoryItem={"Supplier","Consumer","Admin"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-//        selectCategory=(Spinner)findViewById(R.id.selectCategory);
-//        ArrayAdapter<String> spinnerAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,categoryItem);
-//        categoryItem.setAdapter(spinnerAdapter);
-//
+        selectCategory=(Spinner)findViewById(R.id.selectCategory);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.categories,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectCategory.setAdapter(adapter);
 
+        selectCategory.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
