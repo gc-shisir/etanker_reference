acf38bbb163933d4bf364b5e0924b3bccf69eb10
Shisir GC <shisir.gc44@gmail.com>
2020-02-15 13:18:32 +0545

    added firebase

--- app/src/main/java/com/example/etanker/registerActivity.java
+++ app/src/main/java/com/example/etanker/registerActivity.java
@@ -1,40 +1,71 @@
 package com.example.etanker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class registerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-    Spinner selectCategory;
+public class registerActivity extends AppCompatActivity {
+
+    EditText supplierName;
+    EditText supplierEmail;
+    EditText supplierPassword;
+    EditText supplierPhone;
+    EditText supplierTankerCount;
+    Button supplierRegister;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        selectCategory=(Spinner)findViewById(R.id.selectCategory);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.categories,
-                android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        selectCategory.setAdapter(adapter);
+        supplierEmail=(EditText)findViewById(R.id.supplier_email);
+        supplierPassword=(EditText)findViewById(R.id.supplier_password);
+        supplierRegister=(Button)findViewById(R.id.supplier_register);
 
-        selectCategory.setOnItemSelectedListener(this);
+        supplierRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String regEmail=supplierEmail.getText().toString();
+                String regPass=supplierPassword.getText().toString();
+
+                if(TextUtils.isEmpty(regEmail)||TextUtils.isEmpty(regPass)){
+                    Toast.makeText(registerActivity.this, "Please enter the details", Toast.LENGTH_SHORT).show();
+                }else{
+                    registerUser(regEmail,regPass);
+                }
+            }
+        });
 
 
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+    private void registerUser(String email,String pass){
+        fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(registerActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(registerActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(registerActivity.this, "Registration unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
