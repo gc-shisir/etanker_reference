3b2256c8e47ac291c95a361cacad3bb9b0793f13
Shisir GC <shisir.gc44@gmail.com>
2020-02-21 16:31:50 +0545

    supplier authentication registration

--- app/src/main/java/com/example/etanker/loginActivity.java
+++ app/src/main/java/com/example/etanker/loginActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
     TextView userRegister;
+    EditText loginEmail;
+    EditText loginPassword;
+    Button btnLogin;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +27,36 @@ public class loginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         userRegister=(TextView) findViewById(R.id.registerUser);
+        loginEmail=(EditText)findViewById(R.id.login_email);
+        loginPassword=(EditText)findViewById(R.id.login_password);
+        btnLogin=(Button)findViewById(R.id.login_button);
+
+        fAuth=FirebaseAuth.getInstance();
+
         userRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(loginActivity.this, registerActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=loginEmail.getText().toString();
+                String password=loginPassword.getText().toString();
+                login(email,password);
+            }
+        });
+    }
+
+    private void login(String email, String password) {
+        fAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(loginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(loginActivity.this,supplierDashboard.class));
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/etanker/registerActivity.java
+++ app/src/main/java/com/example/etanker/registerActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -19,21 +20,31 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class registerActivity extends AppCompatActivity {
 
-    EditText supplierName;
-    EditText supplierEmail;
-    EditText supplierPassword;
-    EditText supplierPhone;
-    EditText supplierTankerCount;
-    Button supplierRegister;
-    FirebaseAuth fAuth;
+    private EditText supplierName;
+    private EditText supplierEmail;
+    private EditText supplierPassword;
+    private EditText supplierPhone;
+    private EditText supplierTankerCount;
+    private Button supplierRegister;
+    private  FirebaseAuth fAuth;
+    FirebaseFirestore firestoreDB;
+    String userID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
 
         supplierEmail=(EditText)findViewById(R.id.supplier_email);
         supplierPassword=(EditText)findViewById(R.id.supplier_password);
@@ -55,17 +66,40 @@ public class registerActivity extends AppCompatActivity {
 
 
     }
-    private void registerUser(String email,String pass){
-        fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(registerActivity.this, new OnCompleteListener<AuthResult>() {
+
+    private void registerUser(String email, String password) {
+
+        fAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener(registerActivity.this,new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
+
+                    supplierName=(EditText)findViewById(R.id.supplier_name);
+                    supplierPhone=(EditText)findViewById(R.id.supplier_phone);
+                    supplierTankerCount=(EditText)findViewById(R.id.supplier_tanker_count);
+
+                    String fullName=supplierName.getText().toString();
+                    String phoneNumber=supplierPhone.getText().toString();
+                    int count=Integer.parseInt(supplierTankerCount.getText().toString());
+
+                    firestoreDB=FirebaseFirestore.getInstance();
+                    userID=fAuth.getCurrentUser().getUid();
+                    DocumentReference documentReference=firestoreDB.collection("suppliers").document(userID);
+                    Map<String,Object>  supplier=new HashMap<>();
+                    supplier.put("full_name",fullName);
+                    supplier.put("contact_number",phoneNumber);
+                    supplier.put("no_of_tankers",count);
+
+                    documentReference.set(supplier);
+
                     Toast.makeText(registerActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(registerActivity.this, "Registration unsuccessful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(registerActivity.this,loginActivity.class));
+                }
+                else{
+                    Toast.makeText(registerActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
 }
+
